6e61f17e68b7022b6d5f901d867c8341bd9c445b
Allison Smith <asmith@planview.com>
2014-10-27 12:29:00 -0500

    Updated links to css and js to this file

--- date.php
+++ date.php
@@ -3,14 +3,16 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Today's date is <?php echo date("l, F j, Y"); ?></title>
-		<link rel="stylesheet" href="css/style.css">
+		<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/main.js"></script>
+<link href="css/app.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
 		<header>
 			<h1>Welcome</h1>
 		</header>
 		<main>
-			<p>You loaded this page at <?php echo date("g:i a"); ?> on <?php echo date("l, F j, Y") ?>. Thanks for coming</p>
+			<p>You loaded this page at <?php echo date("g:i a"); ?> on <?php echo date("l, F j, Y") ?>.</p>
             <p><a href="index.html">Back to Index</a></p>
 		</main>
 		<script src="main.js"></script>	
